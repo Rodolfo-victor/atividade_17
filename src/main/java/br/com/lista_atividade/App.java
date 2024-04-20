ae4ac3a00d73398322f6d0731873de0ebc9d426a
Rodolfo Victor <rodollfovictor07@gmail.com>
2024-04-20 18:34:43 -0300

    first commit

---
+++ src/main/java/br/com/lista_atividade/App.java
@@ -0,0 +1,13 @@
+package br.com.lista_atividade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
